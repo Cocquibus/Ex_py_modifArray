2add543bcf9c1ee961381556af9d3da2ec2fb509
cocquibus <cocquibus.y@gmail.be>
2022-05-06 13:48:12 +0200

    premiere partie modif tab

---
+++ main.py
@@ -0,0 +1,21 @@
+tab = ["coding11","coding12","coding13"]
+print(tab)
+print(len(tab))
+tab.append("yves")
+tab[0] = "maison"
+for i in tab:
+    print(i)
+
+print(tab)
+tab.remove("coding12")
+print(tab)
+
+print(" ")
+print(" ")
+
+tableau = ["pomme","fraise","melon","citron"]
+print(tableau)
+tableau.pop(0)
+print(tableau)
+tab.append("cerise")
+print(tableau)
